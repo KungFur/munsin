30c0da83e12f2730cf856550d3b9cae5799d6585
register moog <moog621@gmail.com>
2019-09-30 13:35:38 +0200

    oops

--- cleanup.bash
+++ cleanup.bash
@@ -5,7 +5,7 @@ function main {
 	local pycache_directories=($(find . -type d -name __pycache__))
 	if [ ${#pycache_directories[@]} -ne 0 ]; then
 		rm -rf ${pycache_directories[@]}
-	done
+	fi
 }
 
 $@
