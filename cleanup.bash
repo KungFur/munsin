43ebb79f8356caa51067d8a2d18e084ef9e4acf0
moog621@gmail.com <moog621@gmail.com>
2019-09-30 13:11:54 +0200

    added systemd service unit, cleanup script and Debian dependency installer

---
+++ cleanup.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function main {
+	local pycache_directories=($(find . -type d -name __pycache__))
+	if [ ${#pycache_directories[@]} -ne 0 ]; then
+		rm -rf ${pycache_directories[@]}
+	done
+}
+
+$@
