a85289cec7160817a8e3b4345546ddfc754252fd
moog621@gmail.com <moog621@gmail.com>
2021-12-01 20:16:49 +0100

    Added a requirements file to actually let the bot work; cleaned up cleanup.bash; updated INSTALL.debian script

--- cleanup.bash
+++ cleanup.bash
@@ -2,10 +2,7 @@
 set -euo pipefail
 
 function main {
-	local pycache_directories=($(find . -type d -name __pycache__))
-	if [ ${#pycache_directories[@]} -ne 0 ]; then
-		rm -rf ${pycache_directories[@]}
-	fi
+	find . -type d -name __pycache__ -delete
 }
 
 $@
